07394fbcbf758026784e8dc10bbb57ed9d6db399
birbNoobstein <ana.jerina@gmail.com>
2022-04-22 14:10:07 +0200

    Corner bug fixed

--- pathfinding_algorithm.py
+++ pathfinding_algorithm.py
@@ -6,14 +6,14 @@ Created on Mon Apr 11 12:22:49 2022
 """
 import numpy as np
 
-def euclidian(snake, apple):
+def manhattan(snake, apple):
     return np.sqrt(np.sum(np.abs(np.subtract(snake, apple))))
 
 
-def basic_moves(apple, snake_head_loc, full_snake, previous):
+def basic_moves(apple, snake_head_loc, full_snake, previous, size, x, y):
     """ 
         Finds pretty basic path to the apple
-        Uses Euclidian distance and a few restrictions
+        Uses Manhattan distance and a few restrictions
     """
     snake = snake_head_loc
     
@@ -22,30 +22,37 @@ def basic_moves(apple, snake_head_loc, full_snake, previous):
                  'left':np.inf,
                  'down':np.inf,
                  'right':np.inf}
-    if snake[0] < 490 and previous != 'down' and [snake[0], snake[1]-10] not in full_snake:
-        distances['up'] = euclidian(np.array([snake[0], snake[1]-10]), apple)
-    if snake[1] > 10 and previous != 'right'and [snake[0]-10, snake[1]] not in full_snake:
-        distances['left'] = euclidian(np.array([snake[0]-10, snake[1]]), apple)
-    if snake[0] > 10 and previous != 'up'and [snake[0], snake[1]+10] not in full_snake:
-        distances['down'] = euclidian(np.array([snake[0], snake[1]+10]), apple)
-    if snake[1] < 790 and previous != 'left' and [snake[0]+10, snake[1]] not in full_snake:
-        distances['right'] = euclidian(np.array([snake[0]+10, snake[1]]), apple)
+    if snake[1] > 0 and previous != 'down' and [snake[0], snake[1]-size] not in full_snake:
+        distances['up'] = manhattan(np.array([snake[0], snake[1]-size]), apple)
+        
+    if snake[0] > 0 and previous != 'right'and [snake[0]-size, snake[1]] not in full_snake:
+        distances['left'] = manhattan(np.array([snake[0]-size, snake[1]]), apple)
+        
+    if snake[1] < y-size and previous != 'up'and [snake[0], snake[1]+size] not in full_snake:
+        distances['down'] = manhattan(np.array([snake[0], snake[1]+size]), apple)
+        
+    if snake[0] < x-size and previous != 'left' and [snake[0]+size, snake[1]] not in full_snake:
+        distances['right'] = manhattan(np.array([snake[0]+size, snake[1]]), apple)
     
     print(distances)
     
     move = [k for k, v in distances.items() if v == min(distances)]
     if len(move) > 1:
-        if snake[1] == apple[1]-10 or snake[1] == apple[1]+10:
-            if 'left' in move:
+        if 'left' in move and snake[0] > size:
                 return 'left'
+        elif 'right' in move and snake[0] < x-size:
             return 'right'
-        elif snake[0] == apple[0]-10 or snake[0] == apple[0]+10:
-            if 'up' in move:
-                return 'up'
+        elif 'up' in move and snake[1] > size:
+            return 'up'
+        elif 'down' in move and snake[0] < y-size:
             return 'down'
     return min(distances, key=distances.get)
 
 
 
 def astar():
+    pass
+
+
+def hamiltonian():
     pass
--- snackysnake.py
+++ snackysnake.py
@@ -9,6 +9,8 @@ import pygame as pg
 import sys
 import random
 import time
+import numpy as np
+import itertools
     
 from pathfinding_algorithm import basic_moves
 
@@ -18,54 +20,54 @@ class SnackySnake:
         Takes argument pathfinder which defines the algorithm that will be
         choosing the path
     '''
-    def __init__(self, pathfinder):
+    def __init__(self, pathfinder, fullscreen=False):
+        #   Set display window
         self.pathfinder = pathfinder
         self.error_check = pg.init()
         pg.display.set_caption('Snacky Snake')
-        self.game_window = pg.display.set_mode([800, 500])
+        self.x = int(np.floor(pg.display.get_desktop_sizes()[0][0]/20)*20) if fullscreen else 800
+        self.y = int(np.floor(pg.display.get_desktop_sizes()[0][1]/20)*20) if fullscreen else 500
+        self.game_window = pg.display.set_mode([self.x, self.y])
+        
+        #   Set positions
+        self.snake_width = 20 if fullscreen else 10
         self.score = 0
-        self.apple = [200, 150]
-        self.snake = [400, 250]
-        self.snake_full = [[400, 250]]
-        self.snake_tail = [400, 250]
-        self.path = None
+        self.apple = [self.x-self.snake_width, self.y-self.snake_width] #[self.x/2-7*self.snake_width, self.y/2-5*self.snake_width]
+        self.snake = [self.x/2, self.y/2]
+        self.snake_full = [[self.x/2, self.y/2]]
+        self.snake_tail = [self.x/2, self.y/2]
+        
         self.snake_color = [pg.Color(53, 255, 0), pg.Color(28, 192, 192)]
         self.apple_color = pg.Color(255, 0, 0)
+        self.path = None
         self.previous = None
         self.time_ctrl = pg.time.Clock()
         
-    def get_apple(self):
-        return self.apple
-    
     def set_apple(self):
-        self.apple = [random.randrange(1, (800//10)) * 10, random.randrange(1, (500//10)) * 10]
+        """ 
+            Chooses coordinates to where apple moves from list of all positions
+            that are not occupied by snake
+        """
+        pair_list = [l for l in list(itertools.product(range(0, self.x, self.snake_width),
+                                                       range(0, self.y, self.snake_width))) if list(l) not in self.snake_full]
+        self.apple = pair_list[random.randint(0, len(pair_list))]
         
     def set_path(self, previous):
-        self.path = [self.pathfinder(self.apple, self.snake, self.snake_full, previous)]
-        
-    def show_score(self, size, label, midgame=True):
         """ 
-            Displays score in the bottom of the game-window during the game and
-            close to the center at the end of the game
+            Uses given algorithm to find the path to the apple
         """
-        font = pg.font.SysFont('inkfree', size)
-        score_surface = font.render(label + ' : ' + str(self.score), True, pg.Color(255, 255, 255))
-        score_rect = score_surface.get_rect()
-        if midgame == 1:
-            score_rect.midtop = (745, 475)
-        else:
-            score_rect.midtop = (400, 350)
-        self.game_window.blit(score_surface, score_rect)
+        self.path = [self.pathfinder(self.apple, self.snake, self.snake_full, previous, self.snake_width, self.x, self.y)]
         
-    def death_reason(self, reason):
+    def show_text(self, font, size, text, y):
         """ 
-            Displays the reason why you died
+            Used to display endgame text, reason and score
         """
-        font = pg.font.SysFont('inkfree', 20)
-        reason_surface = font.render('Reason : ' + reason, True, pg.Color(255, 255, 255))
-        reason_rect = reason_surface.get_rect()
-        reason_rect.midtop = (400, 300)
-        self.game_window.blit(reason_surface, reason_rect)
+        font = pg.font.SysFont(font, size)
+        surface = font.render(text, True, pg.Color(255, 255, 255))
+        rect = surface.get_rect()
+        rect.midtop = (self.x/2, y)
+        self.game_window.blit(surface, rect)
+
         
     def end_game(self):
         """ 
@@ -75,7 +77,7 @@ class SnackySnake:
         for bodypart in self.snake_full[1:]:
             if self.snake[0] == bodypart[0] and self.snake[1] == bodypart[1]:
                 return 'Crashed into yourself', True
-        if self.snake[0] < 0 or self.snake[0] > 800 or self.snake[1] < 0 or self.snake[1] > 500:
+        if self.snake[0] < 0 or self.snake[0] >= self.x or self.snake[1] < 0 or self.snake[1] >= self.y:
             return 'Crashed in to the wall', True
         return '/', False
     
@@ -84,14 +86,18 @@ class SnackySnake:
             Game termination, displays the YOU DIED text, death reason and 
             final length of the snake
         """
-        my_font = pg.font.SysFont('constantia', 60)
-        game_over_surface = my_font.render('YOU DIED', True, pg.Color(255, 255, 255))
-        game_over_rect = game_over_surface.get_rect()
-        game_over_rect.midtop = (400, 125)
-        self.game_window.fill(pg.Color(0, 0, 0))
-        self.game_window.blit(game_over_surface, game_over_rect)
-        self.show_score(18, 'Final length', midgame=False)
-        self.death_reason(reason)
+        self.show_text('constantia',
+                       60, 
+                       'YOU DIED', 
+                       self.y/5)
+        self.show_text('inkfree',
+                       18, 
+                       'Final length: ' + str(self.score), 
+                       self.y/2)
+        self.show_text('inkfree',
+                       20, 
+                       'Reason : ' + reason, 
+                       self.y/1.6)
         pg.display.flip()
         time.sleep(3)
         pg.quit()
@@ -108,43 +114,40 @@ class SnackySnake:
             for action in path:
                 
                 if action == 'up':
-                    self.snake[1] -= 10
+                    self.snake[1] -= self.snake_width
                 elif action == 'down':
-                    self.snake[1] += 10
+                    self.snake[1] += self.snake_width
                 elif action == 'left':
-                    self.snake[0] -= 10
+                    self.snake[0] -= self.snake_width
                 elif action == 'right':
-                    self.snake[0] += 10
+                    self.snake[0] += self.snake_width
                     
                 self.game_window.fill(pg.Color(0, 0, 0))
                 self.snake_full.insert(0, self.snake.copy())
                 if self.snake[0] == self.apple[0] and self.snake[1] == self.apple[1]:
-                    #print('here', self.snake_full)
                     self.score += 1
                     self.set_apple()
                     for e, bodypart in enumerate(self.snake_full):
-                        pg.draw.rect(self.game_window, self.snake_color[e%2], pg.Rect(bodypart[0], bodypart[1], 10, 10))
+                        pg.draw.rect(self.game_window, self.snake_color[e%2], pg.Rect(bodypart[0], bodypart[1], self.snake_width-2, self.snake_width-2))
                 
                 else :
                     self.snake_full = self.snake_full[0:-1]
-                    #print('snake', self.snake_full)
                     for e, bodypart in enumerate(self.snake_full):
-                        pg.draw.rect(self.game_window, self.snake_color[e%2], pg.Rect(bodypart[0], bodypart[1], 10, 10))
+                        pg.draw.rect(self.game_window, self.snake_color[e%2], pg.Rect(bodypart[0], bodypart[1], self.snake_width-2, self.snake_width-2))
                 
+                print(previous, '-->', action)
                 print(self.apple)
                 print(self.snake)
                 print(self.snake_full, '\n')
                     
-                pg.draw.rect(self.game_window, self.apple_color, pg.Rect(self.apple[0], self.apple[1], 10, 10))
+                pg.draw.rect(self.game_window, self.apple_color, pg.Rect(self.apple[0], self.apple[1], self.snake_width, self.snake_width))
                 
                 previous = action
-                self.show_score(14, 'Length')
                 pg.display.update()
-                self.time_ctrl.tick(150)
+                self.time_ctrl.tick(250)
                 
                 reason, end = self.end_game()
                 if end:
-                    #print(self.score)
                     self.game_window.fill(pg.Color(0, 0, 0))
                     self.game_over(reason)
                     
