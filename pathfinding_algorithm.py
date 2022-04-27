3ecac19af719ac84e4975e572d84de182f28b0e9
71M073J <tc7476@student.uni-lj.si>
2022-04-27 16:49:17 +0200

    cycle generator for hamilton pathfinder, still need to make path skipping and single time path generation

--- pathfinding_algorithm.py
+++ pathfinding_algorithm.py
@@ -4,6 +4,8 @@ Created on Mon Apr 11 12:22:49 2022
 
 @author: Ana
 """
+import random
+
 import numpy as np
 import heapq
 import math
@@ -13,10 +15,16 @@ from collections import defaultdict, deque
 
 def manhattan(snake, apple):
     return np.abs(snake - apple).sum()  # np.sum(np.abs(np.subtract(snake, apple)))
+
+
 def euclidean(snake, apple):
     return np.sqrt(np.square(snake - apple).sum())
+
+
 def diagonal(snake, apple):
     return np.abs(snake - apple).max()
+
+
 def basic_moves(apple, snake_head_loc, full_snake, previous, size, x, y):
     """ 
         Finds pretty basic path to the apple
@@ -56,36 +64,43 @@ def basic_moves(apple, snake_head_loc, full_snake, previous, size, x, y):
 
 
 opposite = {"right": "left", "left": "right", "up": "down", "down": "up"}
+
+
 def coords_add(size):
-    return {"left": (-size, 0), "right": (size, 0), "up":(0, -size), "down": (0, size)}
-def is_dead_end(full_snake, attempt_coords, x, y, size, apple): # TODO
-    empty = (x /size) * (y/size) - len(full_snake)
+    return {"left": (-size, 0), "right": (size, 0), "up": (0, -size), "down": (0, size)}
+
+
+def dead_end_score(full_snake, attempt_coords, x, y, size, apple):  # TODO
+    empty = (x / size) * (y / size) - len(full_snake)
     q = deque()
     q.append(tuple(attempt_coords))
     reachable = 0
     visited = set()
     visited.add(tuple(attempt_coords))
-    must_be_reachable = len(full_snake) #TODO
+    must_be_reachable = len(full_snake)  # TODO
     while q:
-        if len(visited) > must_be_reachable:
-            return False
         cur = q.popleft()
         for move, i in [("left", (-size, 0)), ("right", (size, 0)), ("up", (0, -size)), ("down", (0, size))]:
             next = (cur[0] + i[0], cur[1] + i[1])
             if next not in visited and is_safe_move(full_snake, x, y, move, cur, size, apple)[0]:
                 q.append(next)
                 visited.add(next)
-    return True
+    return len(visited)
+
 
 def is_snake(pos, full_snake):
     full = np.array(full_snake)
     if ((np.array(pos) == full).sum(axis=1) == full.shape[1]).any():  # any -> is snake
         return True
     return False
+
+
 def is_outside_edge(pos, x, y):
     if not (0 <= pos[0] < x) or not (0 <= pos[1] < y):
         return True
     return False
+
+
 def is_safe_move(full_snake, x, y, move, currloc, size, apple):
     # if move "move" is safe, from currloc location
     movloc = []
@@ -98,7 +113,7 @@ def is_safe_move(full_snake, x, y, move, currloc, size, apple):
         return False, -1
     pos = np.array((nextx, nexty))
     full = np.array(full_snake)
-    if ((np.array(pos) == full).sum(axis=1) == full.shape[1]).any(): #any -> is snake
+    if ((np.array(pos) == full).sum(axis=1) == full.shape[1]).any():  # any -> is snake
         return False, -1
     return True, euclidean(movloc, apple)
 
@@ -111,24 +126,25 @@ def astar(apple, snake_head_loc, full_snake, previous, size, x_loc, y_loc):
             total_path.append(cur)
         diff = np.array(total_path[-2]) - np.array(total_path[-1])
         if diff[0] > 0 and previous != "left":
-                return "right"
+            return "right"
         if diff[0] < 0 and previous != "right":
-                return "left"
+            return "left"
         if diff[1] > 0 and previous != "up":
-                return "down"
+            return "down"
         if diff[1] < 0 and previous != "down":
-                return "up"
+            return "up"
         return previous
+
     ...
-    for minmax in [0, 1]:
-        if minmax == 0:
+    for minmax in [0.0, 1.0]:
+        if minmax == 0.0:
             app = tuple(apple)
             heuristic_fun = manhattan
             discovered_squares = []
-            heapq.heappush(discovered_squares, (0, tuple(snake_head_loc)))
+            heapq.heappush(discovered_squares, (0.0, tuple(snake_head_loc)))
             came_from = {}
             cheapest_current_path = defaultdict(lambda: math.inf)  # default length for a missing key - unvisited node
-            cheapest_current_path[tuple(snake_head_loc)] = 0
+            cheapest_current_path[tuple(snake_head_loc)] = 0.0
             heuristic_paths = defaultdict(lambda: math.inf)
             heuristic_paths[tuple(snake_head_loc)] = heuristic_fun(snake_head_loc, apple)
             full = np.array(full_snake)
@@ -141,13 +157,13 @@ def astar(apple, snake_head_loc, full_snake, previous, size, x_loc, y_loc):
                     return reconstruct_path(came_from, current, apple)
                 coords = []  # (-size, 0),(size, 0),(0,-size),(0, size)
                 if current[0] > 0:
-                    coords.append((-size, 0))
+                    coords.append((-size, 0.0))
                 if current[0] < x_loc - size:
-                    coords.append((size, 0))
+                    coords.append((size, 0.0))
                 if current[1] > 0:
-                    coords.append((0, -size))
+                    coords.append((0.0, -size))
                 if current[1] < y_loc - size:
-                    coords.append((0, size))
+                    coords.append((0.0, size))
                 for x, y in coords:
                     neighbour = (current[0] + x, current[1] + y)
                     neighbour_heuristic = cheapest_current_path[current] + size
@@ -164,9 +180,11 @@ def astar(apple, snake_head_loc, full_snake, previous, size, x_loc, y_loc):
                                 heapq.heappush(discovered_squares, (heuristic_paths[neighbour], neighbour))
         else:
             return avoidance_move(full_snake, x_loc, y_loc, previous, snake_head_loc, size, apple)
-        #print("failure")
+        # print("failure")
     # Open set is empty but goal was never reached
     return -1
+
+
 def avoidance_move(full_snake, x_loc, y_loc, previous, snake_head_loc, size, apple):
     moves = []
     neigh = []
@@ -178,28 +196,36 @@ def avoidance_move(full_snake, x_loc, y_loc, previous, snake_head_loc, size, app
         moves.append((is_safe_move(full_snake, x_loc, y_loc, move, snake_head_loc, size, apple), move))
 
     moves = [x for x in moves if x[0][0]]
-    #potentialMoves = []
+    # potentialMoves = []
     if previous == "up":
-        if (is_snake(neigh[0], full_snake) and not is_snake(neigh[1], full_snake))\
+        if (is_snake(neigh[0], full_snake) and not is_snake(neigh[1], full_snake)) \
                 or (is_snake(neigh[6], full_snake) and not is_snake(neigh[7], full_snake)):
-            moves = [m if not is_dead_end(full_snake, snake_head_loc + np.array(coords_add(size)[m[1]]), x_loc, y_loc, size, apple) else
-                     ((False, None), "") for m in moves]
+            moves = [
+                ((m[0][0],
+                  dead_end_score(full_snake, snake_head_loc + np.array(coords_add(size)[m[1]]), x_loc, y_loc, size,
+                                 apple)), m[1]) for m in moves]
     elif previous == "down":
         if (is_snake(neigh[2], full_snake) and not is_snake(neigh[1], full_snake)) \
                 or (is_snake(neigh[8], full_snake) and not is_snake(neigh[7], full_snake)):
-            moves = [m if not is_dead_end(full_snake, snake_head_loc + np.array(coords_add(size)[m[1]]), x_loc, y_loc, size, apple) else
-                     ((False, None), "") for m in moves]
+            moves = [
+                ((m[0][0],
+                  dead_end_score(full_snake, snake_head_loc + np.array(coords_add(size)[m[1]]), x_loc, y_loc, size,
+                                 apple)), m[1]) for m in moves]
     elif previous == "left":
         if (is_snake(neigh[0], full_snake) and not is_snake(neigh[3], full_snake)) \
                 or (is_snake(neigh[2], full_snake) and not is_snake(neigh[5], full_snake)):
-            moves = [m if not is_dead_end(full_snake, snake_head_loc + np.array(coords_add(size)[m[1]]), x_loc, y_loc, size, apple) else
-                     ((False, None), "") for m in moves]
+            moves = [
+                ((m[0][0],
+                  dead_end_score(full_snake, snake_head_loc + np.array(coords_add(size)[m[1]]), x_loc, y_loc, size,
+                                 apple)), m[1]) for m in moves]
     elif previous == "right":
         if (is_snake(neigh[6], full_snake) and not is_snake(neigh[3], full_snake)) \
                 or (is_snake(neigh[8], full_snake) and not is_snake(neigh[5], full_snake)):
-            moves = [m if not is_dead_end(full_snake, snake_head_loc + np.array(coords_add(size)[m[1]]), x_loc, y_loc, size, apple) else
-                     ((False, None), "") for m in moves]
-    #TODO here if it is dead end just reweight the distance heuristic not completely remove the move
+            moves = [
+                ((m[0][0],
+                  dead_end_score(full_snake, snake_head_loc + np.array(coords_add(size)[m[1]]), x_loc, y_loc, size,
+                                 apple)), m[1]) for m in moves]
+
     moves = [x for x in moves if x[0][0]]
     if len(moves) > 0:
         worstMove = max(moves, key=lambda x: x[0][1])
@@ -209,9 +235,108 @@ def avoidance_move(full_snake, x_loc, y_loc, previous, snake_head_loc, size, app
         print("no good safe move found")
         for move in ["up", "down", "left", "right"]:
             moves.append((is_safe_move(full_snake, x_loc, y_loc, move, snake_head_loc, size, apple), move))
-        return max(moves, key=lambda  x:x[0][1])[1]
+        return max(moves, key=lambda x: x[0][1])[1]
         ...
 
-    #return -1
-def hamiltonian():
-    pass
+    # return -1
+
+
+def generate_cyclic_path(startx, starty, size, endx, endy):
+    starting_path = {}
+    last = (startx, starty)
+    x, y = startx, starty
+    for sign in [1, -1]:  # construct basic cycle
+        increment = size * sign
+        while starty - increment <= y < endy - increment:
+            y += increment
+            starting_path[last] = (x, y)
+            last = (x, y)
+        while startx - increment <= x < endx - increment:
+            x += increment
+            starting_path[last] = (x, y)
+            last = (x, y)
+    return starting_path
+
+
+def gen_MST(size, x_loc, y_loc):
+    n = int((x_loc / size) / 2)
+    m = int((y_loc / size) / 2)
+    print(m, n)
+    grid = np.full((m, n), 0.0, dtype="f, f")
+    for i in range(m):
+        for j in range(n):
+            grid[i, j] = (i, j)
+    start = (int(m / 2), int(n / 2))
+    # print(grid, start)
+    visited = set()
+    visited.add(start)
+    q = []
+    heapq.heappush(q, (0, start))
+    while q:
+        d, loc = heapq.heappop(q)
+        y, x = loc
+        # vals = []
+        for d in [(1, 0), (-1, 0), (0, 1), (0, -1)]:
+            cy, cx = y + d[0], x + d[1]
+            if (cy, cx) not in visited:
+                if 0 <= cy < m and 0 <= cx < n:
+                    heapq.heappush(q,
+                                   (random.random() * 0.5 + manhattan(np.array(start), np.array((cy, cx))), (cy, cx)))
+                    visited.add((cy, cx))
+                    grid[cy, cx] = (y, x)
+    cycle = np.full((m * 2, n * 2), -1.0, dtype="f, f")
+    visited = set()
+    visited.add(start)
+    q = deque()
+    q.append(start)
+    while q:
+        i, j = q.popleft() #trenutno polje
+        ds = [(1, 0), (-1, 0), (0, 1), (0, -1)]
+        for d in [(1, 0), (-1, 0), (0, 1), (0, -1)]:
+            cy, cx = i + d[0], j + d[1]#sosedna polja
+            if 0 <= cy < m and 0 <= cx < n and (cy, cx) not in visited:
+                vy, vx = grid[cy, cx]
+                if vy == i and vx == j:  # kaže na trenutno polje
+                    ds.remove(d)
+                    q.append((cy, cx))
+                    visited.add((cy, cx))
+                    diff = (np.array((cy, cx)) - np.array(tuple(grid[cy, cx])))  # določi od kje
+                    if diff[0] == -1:  # iz spodaj
+                        cycle[2 * cy + 2, 2 * cx + 1] = (2 * cy + 1, 2 * cx + 1)
+                        cycle[2 * cy + 1, 2 * cx    ] = (2 * cy + 2, 2 * cx    )
+                    if diff[0] == 1:  # iz zgoraj
+                        cycle[2 * cy - 1, 2 * cx    ] = (2 * cy    , 2 * cx    )
+                        cycle[2 * cy    , 2 * cx + 1] = (2 * cy - 1, 2 * cx + 1)
+                    if diff[1] == -1:  # iz desne
+                        cycle[2 * cy    , 2 * cx + 2] = (2 * cy    , 2 * cx + 1)
+                        cycle[2 * cy + 1, 2 * cx + 1] = (2 * cy + 1, 2 * cx + 2)
+                    if diff[1] == 1:  # iz leve
+                        cycle[2 * cy + 1, 2 * cx - 1] = (2 * cy + 1, 2 * cx    )
+                        cycle[2 * cy    , 2 * cx    ] = (2 * cy    , 2 * cx - 1)
+        for d in ds: #kje neporabljeni premiki
+            if d[0] == 1:  # spodaj
+                if tuple(cycle[2 * i + 1, 2 * j    ]) == (-1 ,-1):
+                    cycle[2 * i + 1, 2 * j] = (2 * i + 1, 2 * j + 1)
+            if d[0] == -1:  # zgoraj
+                if tuple(cycle[2 * i, 2 * j + 1]) == (-1, -1):
+                    cycle[2 * i, 2 * j + 1] = (2 * i, 2 * j)
+            if d[1] == 1:  # desne
+                if tuple(cycle[2 * i + 1, 2 * j + 1]) == (-1, -1):
+                    cycle[2 * i + 1, 2 * j + 1] = (2 * i, 2 * j + 1)
+            if d[1] == -1:  # leve
+                if tuple(cycle[2 * i, 2 * j]) == (-1, -1):
+                    cycle[2 * i, 2 * j] = (2 * i + 1, 2 * j)
+    #print(cycle)
+    import matplotlib.pyplot as plt
+    for i in range(cycle.shape[0]):
+        for j in range(cycle.shape[1]):
+            plt.plot([i, cycle[i, j][0]], [j, cycle[i, j][1]])
+    plt.show()
+
+
+def hamiltonian(apple, snake_head_loc, full_snake, previous, size, x_loc, y_loc):
+    gen_MST(size, x_loc, y_loc)
+
+
+if __name__ == "__main__":
+    hamiltonian(None, None, None, None, 10, 800, 500)
